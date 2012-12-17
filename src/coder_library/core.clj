e35c0007a5a5c413e261ce0f53f13ed3f43b50d5
Humberto Pinheiro <humbhenri@gmail.com>
2012-12-17 21:54:26 -0200

    swing utils
    Taken from Joy of Clojure

--- src/coder_library/core.clj
+++ src/coder_library/core.clj
@@ -1,5 +1,18 @@
 (ns coder-library.core
-  (:gen-class))
+  (:gen-class)
+  (:use [coder-library.swing :only [label button txt shelf stack splitter grid alert jlist]])
+  (:import [javax.swing Box BoxLayout JTextField JPanel
+            JSplitPane JLabel JButton
+            JOptionPane]
+           [java.awt BorderLayout Component GridLayout FlowLayout]
+           [java.awt.event ActionListener]))
+
+(defn make-window []
+  (doto (JFrame. "Coder Libray")
+    (.setContentPane (stack (jlist ["Humberto" "Henrique" "Campos" "Pinheiro"])))
+    (.pack)
+    (.setVisible true)))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
---
+++ src/coder_library/swing.clj
@@ -0,0 +1,62 @@
+(ns coder-library.swing
+  (:gen-class)
+  (:import [javax.swing Box BoxLayout JTextField JPanel JSplitPane JLabel JButton
+            JOptionPane DefaultListModel JList ListSelectionModel JScrollPane]
+           [java.awt BorderLayout Component GridLayout FlowLayout]
+           [java.awt.event ActionListener]))
+
+;;; Joy of Clojure
+(defn shelf [& components]
+  (let [shelf (JPanel.)]
+    (.setLayout shelf (FlowLayout.))
+    (doseq [c components] (.add shelf c))
+    shelf))
+
+(defn stack [& components]
+  (let [stack (Box. BoxLayout/PAGE_AXIS)]
+    (doseq [c components]
+      (.setAlignmentX c Component/CENTER_ALIGNMENT)
+      (.add stack c))
+    stack))
+
+(defn splitter [top bottom]
+  (doto (JSplitPane.)
+    (.setOrientation JSplitPane/VERTICAL_SPLIT)
+    (.setLeftComponent top)
+    (.setRightComponent bottom)))
+
+(defn button [text f]
+  (doto (JButton. text)
+    (.addActionListener
+     (proxy [ActionListener] []
+       (actionPerformed [_] (f))))))
+
+(defn txt [cols t]
+  (doto (JTextField.)
+    (.setColumns cols)
+    (.setText t)))
+
+(defn label [txt] (JLabel. txt))
+
+(defn alert
+  ([msg] (alert nil msg))
+  ([frame msg]
+     (javax.swing.JOptionPane/showMessageDialog frame msg)))
+
+(defn grid [x y f]
+  (let [g (doto (JPanel.)
+            (.setLayout (GridLayout. x y)))]
+    (dotimes [i x]
+      (dotimes [j y]
+        (.add g (f))))
+    g))
+
+(defn jlist [items]
+  (let [model (DefaultListModel.)]
+    (doseq [item items]
+      (.addElement model item))
+    (JScrollPane.
+     (doto (JList. model)
+       (.setSelectionMode ListSelectionModel/SINGLE_INTERVAL_SELECTION)
+       (.setLayoutOrientation JList/VERTICAL)
+       (.setVisibleRowCount -1)))))
