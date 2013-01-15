42081797ae26fe8ee9e86f91ac4df6ff6c560c92
Humberto Pinheiro <humbhenri@gmail.com>
2013-01-14 22:54:34 -0200

    auto sort snippets by most recent first

--- src/coder_library/snippets.clj
+++ src/coder_library/snippets.clj
@@ -1,6 +1,7 @@
 (ns coder-library.snippets
   (:import [java.util Date])
-  (:use [clojure.java.io :as io]))
+  (:use [clojure.java.io :as io]
+        [clojure.string :only [upper-case]]))
 
 
 (def snippets [])
@@ -8,15 +9,21 @@
 
 (defn new-snippet [& args]
   (let [[lang body header] args]
-    (def snippets (conj snippets {:language lang
-                                  :body body
-                                  :header header
-                                  :modification (Date.)}))))
+    (def snippets (->> (conj snippets {:language lang
+                                       :body body
+                                       :header header
+                                       :modification (Date.)})
+                       (sort-by :modification)
+                       (reverse)
+                       (vec)))))
 
 
 (defn get-all-snippets [db-path]
   (try
-    (def snippets (load-file db-path))
+    (def snippets (->> (load-file db-path)
+                       (sort-by :modification)
+                       (reverse)
+                       (vec)))
     snippets
     (catch java.io.FileNotFoundException e
       (def snippets (vector))
@@ -34,6 +41,6 @@
 (defn search [term]
   (let [terms (into [] (.split term "\\s+"))]
     (distinct (for [s snippets t terms
-                    :when (or (.contains (:header s) t)
-                              (.contains (:body s) t))]
+                    :when (or (-> (upper-case (:header s)) (.contains (upper-case t)))
+                              (-> (upper-case (:body s)) (.contains (upper-case t))))]
                 s))))
